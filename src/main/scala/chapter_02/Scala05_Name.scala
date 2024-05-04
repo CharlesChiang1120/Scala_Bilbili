0f19af034d3577bceeac52e83d5b613af3c6911a
Charles Chiang <charleschiang2343@gmail.com>
2024-05-04 14:17:49 +0800

    Scala05_Name

--- src/main/scala/chapter_02/Scala05_Name.scala
+++ src/main/scala/chapter_02/Scala05_Name.scala
@@ -21,7 +21,7 @@ object Scala05_Name {
     val name_e1 = "zhangsan"
     val name$e1 = "zhangsan"
     // val 1name$e1 = "zhangsan"
-    // 如果非要使用關鍵字和標示符重名， 加反引號
+    // 如果非要使用關鍵字和標示符重名，加反引號
     val 'private = "zhangsan"
     // 或區分成大小寫
     val Private = "zhangsan"
