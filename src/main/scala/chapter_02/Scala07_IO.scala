4c2c0addba392fb751794584bdecd7d5c0ad0556
Charles Chiang <charleschiang2343@gmail.com>
2024-05-05 15:31:43 +0800

    Scala07_IO.scala

--- src/main/scala/chapter_02/Scala07_IO.scala
+++ src/main/scala/chapter_02/Scala07_IO.scala
@@ -1,11 +1,13 @@
 package chapter_02
 
+import java.io.PrintWriter
 import scala.io.{BufferedSource, Source}
 
 object Scala07_IO {
   def main(args: Array[String]): Unit = {
 
     // TODO IO
+    // TODO Input
     // 從控制台獲取輸入的數字
     // val age : Int = scala.io.StdIn.readInt()
     // println(age)
@@ -18,5 +20,13 @@ object Scala07_IO {
       println(iter.next())
     }
     source.close()
+
+    // TODO output
+    // Scala 中採用 Java 中的輸出操作
+    val out = new PrintWriter("/Users/charleschiang/Desktop/Udemy/JAVA/Scala-Bilibili/data/test.txt")
+    out.println("test")
+    out.println("scala")
+    out.flush()
+    out.close()
   }
 }
