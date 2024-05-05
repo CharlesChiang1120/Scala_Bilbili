60e0974d71ec54203a43351a4a531bc576648a00
Charles Chiang <charleschiang2343@gmail.com>
2024-05-05 14:47:52 +0800

    Scala06_String

---
+++ src/main/scala/chapter_02/Scala06_String.scala
@@ -0,0 +1,40 @@
+package chapter_02
+
+object Scala06_String {
+  def main(args: Array[String]): Unit = {
+
+    // TODO 字符串
+    // Scala 中沒有字符串，使用就是 Java 字符串
+    // 字符串的方法，都可在 Scala 中使用
+    // val name : String = "ABC"
+
+    // TODO 字符串的拼接問題
+    val name = "zhangsan"
+    val age = 30
+
+    // println("Name : " + name + ", Age : " + age)
+    // JSON 字符串 : 符合 JSON 格式的字符串
+    // {"name" : "zhangsan", "age" : 30}
+    println("{\"name\" : \""+name+"\", \"age\" : "+age+"}")
+
+    // 傳值字符串拼接
+    printf("name=%s\n", name)
+
+    // 插值字符串拼接
+    println(s"name=${name}")
+    println(s"name=$name")
+    println(s"name=${name.substring(0, 1)}")
+    println(s"{\"name\" : \"${name}\", \"age\" : ${age}}")
+
+    // 多行字符串
+    // 這裡的豎線表示頂格符
+    println(
+      """
+        | hello
+        | scala
+        |""".stripMargin)
+
+
+
+  }
+}
