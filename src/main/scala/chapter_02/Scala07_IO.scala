b6a71e8ae876348e2131ca8a9950aab333582348
Charles Chiang <charleschiang2343@gmail.com>
2024-05-05 15:27:44 +0800

    Remove cache

--- src/main/scala/chapter_02/Scala07_IO.scala
+++ src/main/scala/chapter_02/Scala07_IO.scala
@@ -1,58 +1,22 @@
 package chapter_02
 
-object Scala06_String {
+import scala.io.{BufferedSource, Source}
+
+object Scala07_IO {
   def main(args: Array[String]): Unit = {
 
-    // TODO 字符串
-    // Scala 中沒有字符串，使用就是 Java 字符串
-    // 字符串的方法，都可在 Scala 中使用
-    // val name : String = "ABC"
+    // TODO IO
+    // 從控制台獲取輸入的數字
+    // val age : Int = scala.io.StdIn.readInt()
+    // println(age)
 
-    // TODO 字符串的拼接問題
-    val name = "zhangsan"
-    val age = 30
-
-    // println("Name : " + name + ", Age : " + age)
-    // JSON 字符串 : 符合 JSON 格式的字符串
-    // {"name" : "zhangsan", "age" : 30}
-    println("{\"name\" : \""+name+"\", \"age\" : "+age+"}")
-
-    // 傳值字符串拼接
-    printf("name=%s\n", name)
-
-    // 插值字符串拼接
-    println(s"name=${name}")
-    println(s"name=$name")
-    println(s"name=${name.substring(0, 1)}")
-    println(s"{\"name\" : \"${name}\", \"age\" : ${age}}")
-
-    // 多行字符串，主要用於 SQL 和 JSON 格式字符串
-    // 這裡的豎線表示頂格符
-    println(
-      """
-        | hello
-        | scala
-        |""".stripMargin)
-
-    // 想把頂格符換成 #
-    println(
-      """
-        # hello
-        # scala
-        #""".stripMargin('#'))
-
-    // 主要用於 SQL 和 JSON 格式字符串
-    println(
-      s"""
-        | {"name" : "${name}", "age" : ${age}}
-        |""".stripMargin)
-
-    println(
-      s"""
-         | SELECT
-         | *
-         | FROM user
-         | WHERE id = 1
-         |""".stripMargin)
+    // 從文件中獲取數據
+    // Scala 獲取文件的數據，需要採用特殊的物件
+    val source: BufferedSource = Source.fromFile("/Users/charleschiang/Desktop/Udemy/JAVA/Scala-Bilibili/data/word.txt")
+    val iter: Iterator[String] = source.getLines()
+    while (iter.hasNext){
+      println(iter.next())
+    }
+    source.close()
   }
 }
