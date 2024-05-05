b8420fabb22b48e0aa90bddd3b6002944a762fd6
Charles Chiang <charleschiang2343@gmail.com>
2024-05-05 16:00:13 +0800

    Scala08_Net_Client & Scala08_Net_Server

--- src/main/scala/chapter_02/Scala08_Net_Server.scala
+++ src/main/scala/chapter_02/Scala08_Net_Server.scala
@@ -7,6 +7,9 @@ object Scala08_Net_Server {
   def main(args: Array[String]): Unit = {
 
     // TODO 創建服務器
+    // 網絡通訊中沒有辦法傳輸物件，數字這樣的內容
+    // 可以在網絡中傳遞字節碼，那麼可以將物件轉換為字節碼，這個過程稱之為序列化
+    // 將字節碼轉換成物件，則稱之為反序列化
     val server = new ServerSocket(9999)
     println("服務器啟動成功，等待客戶端的連結…")
     val client : Socket = server.accept()
