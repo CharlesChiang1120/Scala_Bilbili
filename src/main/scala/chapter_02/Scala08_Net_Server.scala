cc1f50d18bfe382f41389c3a57e7bbcba5546be1
Charles Chiang <charleschiang2343@gmail.com>
2024-05-05 15:38:40 +0800

    Scala07_IO.scala

--- src/main/scala/chapter_02/Scala08_Net_Server.scala
+++ src/main/scala/chapter_02/Scala08_Net_Server.scala
@@ -14,19 +14,19 @@ object Scala07_IO {
 
     // 從文件中獲取數據
     // Scala 獲取文件的數據，需要採用特殊的物件
-    // val source: BufferedSource = Source.fromFile("../../../../data/word.txt")
-    // val iter: Iterator[String] = source.getLines()
-    // while (iter.hasNext){
-    //  println(iter.next())
-    // }
-    // source.close()
+    val source: BufferedSource = Source.fromFile("/Users/charleschiang/Desktop/Udemy/JAVA/Scala-Bilibili/data/word.txt")
+    val iter: Iterator[String] = source.getLines()
+    while (iter.hasNext){
+      println(iter.next())
+    }
+    source.close()
 
     // TODO output
     // Scala 中採用 Java 中的輸出操作
-    // val out = new PrintWriter("../../../../data/data/test.txt")
-    // out.println("test")
-    // out.println("scala")
-    // out.flush()
-    // out.close()
+    val out = new PrintWriter("/Users/charleschiang/Desktop/Udemy/JAVA/Scala-Bilibili/data/test.txt")
+    out.println("test")
+    out.println("scala")
+    out.flush()
+    out.close()
   }
 }
