f4de1dab26512c1a46f299bde865ff0e177233b9
Charles Chiang <charleschiang2343@gmail.com>
2024-05-19 13:51:22 +0800

    Scala09_DataType_2.scala

---
+++ src/main/scala/chapter_02/Scala09_DataType_2.scala
@@ -0,0 +1,14 @@
+package chapter_02
+
+object Scala09_DataType_2 {
+  def main(args: Array[String]): Unit = {
+
+    // TODO
+    val s : String = null
+    val b : Byte = 10
+    val i : Int = b
+    println(i)
+
+
+  }
+}
