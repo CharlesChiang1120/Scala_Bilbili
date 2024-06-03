2604d281942a40a9d69369be5294c3a7a000089b
Charles Chiang <charleschiang2343@gmail.com>
2024-06-03 09:39:44 +0800

    Scala09_DataType_2

--- src/main/scala/chapter_02/Scala09_DataType_2.scala
+++ src/main/scala/chapter_02/Scala09_DataType_2.scala
@@ -5,10 +5,11 @@ object Scala09_DataType_2 {
 
     // TODO
     val s : String = null
+
+    // Byte 和 Int 兩個類型之間沒有任何關係，所以不應該能夠轉換
+    // Scala 底層對類型進行了轉換，所以代碼上看不出來，稱之為隱式（自動）轉換
     val b : Byte = 10
     val i : Int = b
     println(i)
-
-
   }
 }
