349d4ecbd0dddf21b6611b0b15c5e8c208436208
Charles Chiang <charleschiang2343@gmail.com>
2024-06-03 09:48:36 +0800

    Scala09_DataType_3

---
+++ src/main/scala/chapter_02/Scala09_DataType_3.scala
@@ -0,0 +1,21 @@
+package chapter_02
+
+object Scala09_DataType_3 {
+  def main(args: Array[String]): Unit = {
+
+    // TODO
+    // Scala 支持隱式轉換，但也可以顯式轉換
+    //    val b : Byte = 10
+    //    val i : Int = b.toInt
+    //    println(i)
+
+    // TODO 常量是可以在編譯時計算的，不涉及運行時的類型轉換
+    //    val c : Char = ('A' + 1)
+    //    println(c)
+
+    //    val i : Int = 10
+    //    val c : Char = i
+    //    println(i)
+
+  }
+}
