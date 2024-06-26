858d022929669c7f292abf6687684d42bed01d5b
Charles Chiang <charleschiang2343@gmail.com>
2024-06-26 21:46:37 +0800

    TestOper.java

--- src/test/scala/TestOper.java
+++ src/test/scala/TestOper.java
@@ -3,8 +3,12 @@ public class TestOper {
 
         // 賦值運算符 : 將等號右邊的計算結果給左邊
         int i = 0;
-        int j = i++;
+        // int j = i++;
+        // ++放置在變量的後面，先賦值再加一，如果放置在前面，先加一，再賦值
+        // ++運算不是原子性運算，所以會有多步操作，多個步驟間會存在臨時計算結果
+        // 計算過程中應該存在臨時變量
+        i = i++; // _tmp = i = 0; i = 0 + 1 = 1; i = _tmp = 0
         System.out.println("i = " + i); // 1
-        System.out.println("j = " + j); // 0
+        // System.out.println("j = " + j); // 0
     }
 }
