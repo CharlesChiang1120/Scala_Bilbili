79ff5e4567d74061f66d64ed54c834e009737c66
Charles Chiang <charleschiang2343@gmail.com>
2024-06-28 09:36:31 +0800

    Scala03_Oper

--- src/main/scala/chapter_03/Scala03_Oper.scala
+++ src/main/scala/chapter_03/Scala03_Oper.scala
@@ -9,10 +9,11 @@ object Scala03_Oper {
       // byte => 8位 => 0000 0001
       //             => 0000 0101
       // HashMap 的數據如何存放底層結構中
+      // 存儲容量必須為2的N次方，所以擴容時必須為2倍
       // key => key.hashcode => hash => index
       // index = hash(key.hashCode()) & (length - 1)
       // Kafka 中的數據如何存放在 topic 的 partition 中
-      // key => 取餘 => partition
+      // key => %取餘 => partition
 
 
     }
