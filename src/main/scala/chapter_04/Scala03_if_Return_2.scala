4485ba54f90e240d8bdb9a4a5fc1731ce6a1c7b9
Charles Chiang <charleschiang2343@gmail.com>
2024-06-28 13:41:59 +0800

    Scala03_if_Return_2

--- src/main/scala/chapter_04/Scala03_if_Return_2.scala
+++ src/main/scala/chapter_04/Scala03_if_Return_2.scala
@@ -1,18 +1,22 @@
 package chapter_04
 
-object Scala03_if_Return_1 {
+object Scala03_if_Return_2 {
   def main(args: Array[String]): Unit = {
 
     // TODO 分支流程
-    val age = 30
-    val result : Any = if (age == 30) {
-      1
-    } else {
-      "zhangsan"
-    }
-    // Any 類型的數據無法進行數據增加
-    // println(result + 1)
+    // Scala 中分支流程判斷沒有 三元運算符，可以採用 if 來代替
+    // Scala 中也沒有 switch 語法
+    // 如果表達式中的邏輯代碼只有一行，可以省略大括號
+    // xxxx ? a : b
 
+    val age = 30
+//    val result = if (age == 30) {
+//      "30"
+//    } else {
+//      "other"
+//    }
+    val result = if (age ==  30) "30" else "other"
+    println(result)
   }
 
 }
