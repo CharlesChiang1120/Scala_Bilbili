769823b8981dddcd7c9ea5d526bffe8c40e32248
Charles Chiang <charleschiang2343@gmail.com>
2024-06-28 09:33:47 +0800

    Scala03_Oper

--- src/main/scala/chapter_03/Scala03_Oper.scala
+++ src/main/scala/chapter_03/Scala03_Oper.scala
@@ -3,5 +3,17 @@ package chapter_03
 object Scala03_Oper {
     def main(args: Array[String]): Unit = {
 
+      // TODO 運算符
+      // 位運算符
+      // 二進制運算符
+      // byte => 8位 => 0000 0001
+      //             => 0000 0101
+      // HashMap 的數據如何存放底層結構中
+      // key => key.hashcode => hash => index
+      // index = hash(key.hashCode()) & (length - 1)
+      // Kafka 中的數據如何存放在 topic 的 partition 中
+      // key => 取餘 => partition
+
+
     }
   }
