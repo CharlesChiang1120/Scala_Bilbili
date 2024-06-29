5a7cef1742250347a285f0e181862d8a5e098f27
Charles Chiang <charleschiang2343@gmail.com>
2024-06-29 10:49:30 +0800

    TestFor

--- src/test/scala/TestFor.java
+++ src/test/scala/TestFor.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class TestFor {
+public class TestFor {
+    public static void main(String[] args) {
+        // 0, 1, 2, 3, 4
+        for ( int i = 0; i < 5; i++) {
+            if (i == 3){
+                break;
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
 }
