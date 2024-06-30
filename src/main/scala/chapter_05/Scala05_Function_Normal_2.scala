3f04c29a2c42bd9cf584664e1f7b92ddba11f6b7
Charles Chiang <charleschiang2343@gmail.com>
2024-06-30 17:04:30 +0800

    chapter_05/Scala05_Function_Normal_2

--- src/main/scala/chapter_05/Scala05_Function_Normal_2.scala
+++ src/main/scala/chapter_05/Scala05_Function_Normal_2.scala
@@ -1,5 +1,15 @@
 package chapter_05
 
 object Scala05_Function_Normal_2 {
+  def main(args: Array[String]): Unit = {
 
+    // TODO 函數是編成語言 - 普通版
+
+    def test1(password: String = "000000", name: String): Unit = {
+      println(s"註冊用戶: ${name}, 密碼: ${password}")
+    }
+
+    // 如果想要改變參數的傳值順序，那麼可以採用帶名參數
+    test1(name = "Zhangsan")
+  }
 }
