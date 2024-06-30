54fca48b1ece28795512774a241fe74433973dac
Charles Chiang <charleschiang2343@gmail.com>
2024-06-30 16:32:08 +0800

    chapter_03/Scala05_Oper

---
+++ src/main/scala/chapter_03/Scala05_Oper.scala
@@ -0,0 +1,15 @@
+package chapter_03
+
+import scala.language.postfixOps
+
+object Scala05_Oper {
+    def main(args: Array[String]): Unit = {
+
+      // TODO 運算符
+      val r = 1.+(1)
+      val r1 = 1 + 1
+      println(r1)
+
+
+    }
+  }
