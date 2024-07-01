10db085b53fb54165c6e7c911d449ef05dd53c97
Charles Chiang <charleschiang2343@gmail.com>
2024-07-01 13:09:03 +0800

    chapter_05/Scala07_Hell_5_Test

---
+++ src/main/scala/chapter_05/Scala07_Hell_5_Test.scala
@@ -0,0 +1,16 @@
+package chapter_05
+
+object Scala07_Hell_5_Test {
+  def main(args: Array[String]): Unit = {
+
+    // TODO 函數是編成語言 - 地獄版
+
+    def calc( x: Int, f: (Int, Int) => Int, y: Int): Int = {
+      f(x, y)
+    }
+
+    val result = calc(5, _*_, 3)
+    println(result)
+
+  }
+}
