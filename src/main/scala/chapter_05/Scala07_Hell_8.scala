bdab2ed9c889bac56e751c5101b479aeabbd076a
Charles Chiang <charleschiang2343@gmail.com>
2024-07-01 13:35:11 +0800

    chapter_05/Scala07_Hell_8

--- src/main/scala/chapter_05/Scala07_Hell_8.scala
+++ src/main/scala/chapter_05/Scala07_Hell_8.scala
@@ -1,39 +1,19 @@
 package chapter_05
 
-object Scala07_Hell_7 {
+object Scala07_Hell_8 {
   def main(args: Array[String]): Unit = {
 
     // TODO 函數是編成語言 - 地獄版
 
-//    def outer() = {
-//      def inner(): Unit = {
-//        println("inner.....")
-//      }
-//      inner _
-//    }
-
-    // val f = outer()
-    // f()
-    // javascript: 函數式編程語言
-    // test()()()()()()
-//    outer()()
-
-
     def outer(x: Int) = {
-      def mid(f: (Int, Int) => Int) = {
         def inner(y : Int): Int = {
-          f(x, y)
+          x + y
         }
         inner _
-      }
-      mid _
     }
 
-//    val mid = outer(10)
-//    val inner = mid(_+_)
-//    val result = inner(20)
-//    println(result)
-    println(outer(10)(_ + _)(20))
-
+    val inner = outer(10)
+    val result = inner(20)
+    println(result)
   }
 }
