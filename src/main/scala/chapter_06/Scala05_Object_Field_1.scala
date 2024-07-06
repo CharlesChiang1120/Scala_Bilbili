0f8450ed484b9491d9052ca9deec233b97ba268f
Charles Chiang <charleschiang2343@gmail.com>
2024-07-06 17:10:32 +0800

    chapter_06/Scala05_Object_Field_1

--- src/main/scala/chapter_06/Scala05_Object_Field_1.scala
+++ src/main/scala/chapter_06/Scala05_Object_Field_1.scala
@@ -10,6 +10,7 @@ object Scala05_Object_Field_1 {
       val user = new User()
       user.name = "lisi"
       println(user.name)
+//      user.age = 40
 
 //      user.setName()
 //      user.getName()
@@ -23,9 +24,11 @@ object Scala05_Object_Field_1 {
     //     屬性私有化，提供公共的 set, get 方法，方法以 set, get 開頭
     // Scala 中物件屬性不遵循 Bean 開發規範，這樣，和其他技術框架集成的時候，會出現很多問題
     // 所以為了能夠和其他框架相集成， Scala 提供了一個註解，解決 Bean 規範的問題
+    // 屬性的權限修飾符，如果採用 private ，那麼編譯時 set, get 方法也是 private
+    // 增加 @BeanProperty 注視的屬性不能聲明 private 權限
     class User {
         @BeanProperty var name: String = _
-        val age: Int = 30
+        private var age: Int = 30
         val email = "zhangsan@zhangsan"
     }
 }
