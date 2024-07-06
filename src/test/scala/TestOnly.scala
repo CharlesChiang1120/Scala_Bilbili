aaf414960fd8a1fff41e14d013c91bb4d7541e03
Charles Chiang <charleschiang2343@gmail.com>
2024-07-06 10:01:25 +0800

    test/scala/TestOnly

--- src/test/scala/TestOnly.scala
+++ src/test/scala/TestOnly.scala
@@ -1,3 +1,17 @@
 object TestOnly {
+  def main(args: Array[String]): Unit = {
+
+    def fun1(): String = {
+      "aaaa"
+    }
+
+    val a = fun1
+    val b = fun1 _
+    val c: () => Unit = fun1
+
+    println(a)
+    println(b)
+    println(c)
+  }
 
 }
