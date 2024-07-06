04a7a6cdc9167b4e28437dfb8019168584866539
Charles Chiang <charleschiang2343@gmail.com>
2024-07-06 14:52:07 +0800

    Add case to test import

--- src/test/scala/TestImport.java
+++ src/test/scala/TestImport.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class TestImport {
+import java.util.ArrayList;
+
+import java.util.*;
+
+public class TestImport {
+    public static void main(String[] args) {
+        new ArrayList();
+    }
 }
