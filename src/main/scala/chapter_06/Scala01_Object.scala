362c8dc98f2086c4779378c5455f0353ae9d6a13
Charles Chiang <charleschiang2343@gmail.com>
2024-07-06 11:34:08 +0800

    chapter_06/Scala01_Object

---
+++ src/main/scala/chapter_06/Scala01_Object.scala
@@ -0,0 +1,26 @@
+package chapter_06
+
+object Scala01_Object {
+  def main(args: Array[String]): Unit = {
+    // TODO 物件導向編程
+    // Scala 語言是基於 Java 語言開發的，所以基本物件導向的語法是相同的
+    // 但是 Scala 語言是完全物件導向的，所以很多 Java 的語法是不是用的，所以學習時要區分
+    /*
+     TODO Java:
+
+     package  com.atguigu.xxxxx.yyyyy
+     import java.util.*;
+
+     public class User {
+        private String name;
+        public void test()
+        }
+      }
+
+     User user = new User();
+     user.test();
+
+     TODO Scala: 學習時，語法順序按照代碼開發順序學習
+     */
+  }
+}
