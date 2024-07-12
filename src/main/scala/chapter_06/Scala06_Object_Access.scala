9c0bbe29e71dc57bd06e43dcef875f6a5c046c13
Charles Chiang <charleschiang2343@gmail.com>
2024-07-12 09:09:43 +0800

    chapter_06/Scala06_Object_Access

--- src/main/scala/chapter_06/Scala06_Object_Access.scala
+++ src/main/scala/chapter_06/Scala06_Object_Access.scala
@@ -8,7 +8,7 @@ object Scala06_Object_Access {
         // TODO 物件導向編程 - 訪問權限
         //    Scala 中的訪問權限和 Java 類似，也有四種不同的權限
         // 1. private: 私有，同類
-        // 2. private[包名]: 包私有，同類，同包
+        // 2. private[包名]: 包私有，同類，同包(子包)
         // 3. protected: 受保護的，同類，同包，子類
         // 4. (default): 公共的，任何地方都可以使用
     }
@@ -35,7 +35,30 @@ object Scala06_Object_Access {
 
         }
     }
+
+    class Student extends User{
+        def test1(): Unit = {
+            val user = new User()
+            // println(user.name) // X
+            // println(user.age)
+            println(user.email) // 子類
+            println(user.address)
+        }
+    }
 }
 
+package subchapter06{
+    class SubClasee {
+
+        import chapter_06.Scala06_Object_Access.User
+        def test(): Unit = {
+            val user = new User()
+            // println(user.name) // X
+            println(user.age)
+            // println(user.email) // X
+            println(user.address)
+        }
+    }
+}
 
 
