58c21c51f69e1535706eb38fe63217b47f05475d
Charles Chiang <charleschiang2343@gmail.com>
2024-07-14 09:11:27 +0800

    chapter_06/Scala09_Object_Instance_Method

--- src/main/scala/chapter_06/Scala09_Object_Instance_Method.scala
+++ src/main/scala/chapter_06/Scala09_Object_Instance_Method.scala
@@ -1,29 +1,33 @@
 package chapter_06
 
-object Scala08_Object_Instance {
+object Scala09_Object_Instance_Method {
     def main(args: Array[String]): Unit = {
 
-      // TODO 物件導向編程 - 實例化 - 物件
-      // 1. 可以通過 new 構建物件
+      // TODO 物件導向編程 - 實例化 - 物件的構造方法
+      // 1. Java 中類的構造方法默認由 JVM 提供的: 無參，公共的
 //      val user = new User()
 
-      // 2. 可以通過反射構建物件
-      // 3. clone
-      // 4. 反序列化
-      // Scala 中可以使用 object 關鍵字構建單例物件
-      // println(Scala08_Object_Instance)
-      // 6. Scala 中有一個方法可以構建物件，實際上採用的就是 new，但是編譯器可以動態識別
-      // apply 方法可以省略，後面的集合物件基本上就是採用這種方式構建
-      val user = User.apply()
-      val user1 = User()
-      println(user)
-      println(user1)
-    }
-   class User {
+      // 2. Scala 中類的構造方法默認由 JVM 提供，無參，公共的，小括號可以省略
+//      val user1 = new User
 
-   }
-   object User {
-    def apply(): User = new User()
+      // 3. 如果開發人員給類提供了構造方法(任意)，JVM 不會提供默認的構造方法
+      // 4. new 構建物件時，應該執行構造方法
+      // 5. Java 中構造方法和類的名稱是一致的，但是 Scala 中構造方法和類的名稱沒有關係
+//      user1.User()
+      // 6. Scala 是完全面向函數式編程語言，所以萬物皆函數，聲明類就等同於聲明函數
+      //    聲明類的同時所聲明的函數其實就是構造函數，當 new 的時候，函數就會執行
+      // 7. Scala 的構造函數主要功能就是類的實例化(類主體內容的執行)
+      // 8. 構造方法可以傳遞參數
+      val user = new User("zhangsan")
+
+
+    }
+   class User(name: String) {
+     // 類的主體 & 構造函數體
+     println("user instance...")
+//     def User(): Unit = {
+//       println("user instance...")
+//     }
 
    }
 
