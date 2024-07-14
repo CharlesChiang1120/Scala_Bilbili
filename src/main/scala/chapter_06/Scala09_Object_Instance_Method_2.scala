26fed2eb78aff9961c4b32651c4af19ffdc45c66
Charles Chiang <charleschiang2343@gmail.com>
2024-07-14 10:10:59 +0800

    chapter_06/Scala09_Object_Instance_Method_2

--- src/main/scala/chapter_06/Scala09_Object_Instance_Method_2.scala
+++ src/main/scala/chapter_06/Scala09_Object_Instance_Method_2.scala
@@ -1,37 +1,22 @@
 package chapter_06
 
-object Scala09_Object_Instance_Method_1 {
+object Scala09_Object_Instance_Method_2 {
     def main(args: Array[String]): Unit = {
 
       // TODO 物件導向編程 - 實例化 - 物件的構造方法
-      // Java 中的構造方法可以重載
-      // Scala 中的構造方法如何重載的？
-      // TODO Scala 中的構造方法分兩大類
-      // 1. 主構造方法: 完成類的初始化
-      // 2. 輔助構造方法: 在類的初始化後，完成類的輔助功能，屬性賦值
-      //                輔助構造方法必須直接或間接地調用主構造方法
-      //                輔助構造方法有重載的概念，但是主構造方法沒有
-      //                輔助構造方法採用 this 關鍵字聲明
-      //                多個輔助構造方法在調用時，需要提前聲明
+      // 構造方法為什麼要傳參數？
+      // 將類的外部數據傳遞到類的內部，實現數據的賦值，給類的屬性賦值
+      // Scala 為了開發方便，可以在構造參數前增加 var, val 讓參數作為類的的屬性存在
+      val user = new User("zhangsan")
+      user.name
+    }
 
-      // 11111 22222 44444 33333
-      val user = new User("zhangsan",30)
-
+    class User(var name: String) {
 
 
     }
-   class User() {
-     println("11111")
-     def this(name: String)= {
-       this()
-       println("22222")
-     }
-     def this(name: String, age: Int) = {
-       this(name)
-       println("33333")
-     }
-     println("444444")
-   }
+
+
 
 }
 
