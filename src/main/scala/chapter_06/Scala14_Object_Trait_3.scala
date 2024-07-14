2f9d4f1337298db8f20c68837783adc04090b57d
Charles Chiang <charleschiang2343@gmail.com>
2024-07-14 16:49:37 +0800

    chapter_06/Scala14_Object_Trait_3

--- src/main/scala/chapter_06/Scala14_Object_Trait_3.scala
+++ src/main/scala/chapter_06/Scala14_Object_Trait_3.scala
@@ -1,40 +1,47 @@
 package chapter_06
 
-object Scala14_Object_Trait_2 {
+object Scala14_Object_Trait_3 {
   def main(args: Array[String]): Unit = {
 
     // TODO 物件導向編程 - 特徵的執行順序
-    // 1. 將 trait 理解為抽象類，所以 trait的初始化順序應該優於當前的類
-    //    Trait > current Class
-    val user = new User()
-    // 2. 如果類同時有父類和特質，那麼父類的初始化優先
-    //    Parent Class > Trait > current Class
-    // 3. 如果類同時有多個特質
-    //    從左到右依次執行初始化
-    //    Left Trait > Right Trait
-    // 4. 如果父類和子類同時有 Trait
-    //    Parent Trait > Parent Class > Current Trait > Current Class
-    // 5. 在同一個構建過程中，初始化只會執行一次
+    // 類混入特質時，也存在鑽石問題，但是 Scala 採用了一種特殊的方式來解決
+    // 1. 為什麼混入特質
+    //    增加約束
+    //    功能的補充和疊加
+    // 2. 如果混入了多個相同的功能，那麼就需要一個執行的順序，否則會出現錯誤
 
+    // Scala 如何解決鑽石問題:
+    // 將多個功能混入在一起，按照特定的執行順序進行疊加調用
+    // 這裡的特定執行順序其實就是初始化順序的反向操作
+
+    val mysql = new MySQL
+    mysql.operateData()
 
   }
-  trait MyTrait {
-    println("11111")
-  }
-  trait MyTrait1{
-    println("22222")
-  }
-  class Parent extends MyTrait1 {
-    println("33333")
+  trait Operate {
+    def operateData(): Unit = {
+      // TODO 操作數據
+      println("進行數據處理")
     }
-  class User extends Parent with MyTrait with MyTrait1 {
-    println("44444")
   }
-  // 2
-  // 3
-  // 1
-  // 2 // 在同一個構建過程中，初始化只會執行一次
-  // 4
+  trait Database extends Operate {
+    override def operateData(): Unit = {
+      // TODO 操作數據
+      print("向數據庫中")
+      super.operateData()
+    }
+  }
+  trait Log extends Operate {
+    override def operateData(): Unit = {
+      // TODO 操作數據
+      print("向日誌文件中")
+      // 這裡的 super 不是表示父特質，而是表示疊加功能後的上一級
+      super.operateData()
+    }
+  }
+  class MySQL extends Database with Log {
+
+  }
 }
 
 
