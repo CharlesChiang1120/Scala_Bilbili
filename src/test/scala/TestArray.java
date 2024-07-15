e4a39c666dfc54d0565840ea73c98abc8a27da82
Charles Chiang <charleschiang2343@gmail.com>
2024-07-15 08:53:37 +0800

    TestArray

--- src/test/scala/TestArray.java
+++ src/test/scala/TestArray.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class TestArray {
+public class TestArray {
+    public static void main(String[] args) {
+        String[] strs = new String[3];
+        // 修改數據
+        strs[0] = "zhangsan";
+        strs[1] = "lisi";
+        strs[2] = "wangwu";
+        System.out.println(strs);
+    }
 }
