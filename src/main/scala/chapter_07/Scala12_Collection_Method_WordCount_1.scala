77724514f682e84328e1bfab847b2549039b5ef8
Charles Chiang <charleschiang2343@gmail.com>
2024-09-15 21:59:27 +0800

    chapter_07/Scala12_Collection_Method_WordCount_1

---
+++ src/main/scala/chapter_07/Scala12_Collection_Method_WordCount_1.scala
@@ -0,0 +1,37 @@
+package chapter_07
+
+import scala.io.{BufferedSource, Source}
+
+object Scala12_Collection_Method_WordCount_1 {
+  def main(args: Array[String]): Unit = {
+
+    val source: BufferedSource = Source.fromFile("data/word.txt")
+    val dataList: List[String] = source.getLines().toList
+    source.close()
+
+    val top3 = dataList
+      .flatMap(
+          line => {
+            line.split(" ")
+          }
+        ).map(
+            word => {
+              (word, 1)
+            }
+        ).groupBy (
+            kv => {
+              kv._1
+            }
+        ).mapValues(
+          list => {
+            list.size
+          }
+        ).toList.sortBy(
+          wc => {
+            wc._2
+          }
+        )(Ordering.Int.reverse).take(3)
+
+    println(top3)
+  }
+}
