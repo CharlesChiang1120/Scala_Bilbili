db36256196e6a6198e66813ca7932861075c24e8
Charles Chiang <charleschiang2343@gmail.com>
2024-09-15 14:22:20 +0800

    chapter_07/Scala09_Collection_1

---
+++ src/main/scala/chapter_07/Scala09_Collection_1.scala
@@ -0,0 +1,17 @@
+package chapter_07
+
+object Scala09_Collection_1 {
+  def main(args: Array[String]): Unit = {
+
+    // TODO Option 的問題
+    val opt = Some(10)
+    val opt1 = Option(10)
+    val opt2 = None
+    val opt3 = Option(null)
+    println(opt)
+    println(opt1)
+    println(opt2)
+    println(opt3.getOrElse(0))
+
+  }
+}
