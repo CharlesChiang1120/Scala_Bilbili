91c1aed92a525014f75180a091df46ff25ee362b
Charles Chiang <charleschiang2343@gmail.com>
2024-09-15 17:33:01 +0800

    chapter_07/Scala11_Collection_Method_4

--- src/main/scala/chapter_07/Scala11_Collection_Method_4.scala
+++ src/main/scala/chapter_07/Scala11_Collection_Method_4.scala
@@ -1,31 +1,41 @@
 package chapter_07
 
-object Scala11_Collection_Method_3 {
+import scala.collection.mutable.ListBuffer
+
+object Scala11_Collection_Method_4 {
   def main(args: Array[String]): Unit = {
 
     // TODO 集合 - 通用的方法 - 會產生新的集合
-    val list1 = List(1, 2, 3, 4, 5, 6)
-    val list2 = List(3, 4, 5, 6)
+    val list1 = List(1, 2, 3, 4)
 
-    val set1 = Set(1, 2, 3, 4, 5, 6)
-    val set2 = Set(3, 4, 5, 6, 7, 8)
+    // List(2, 4, 6, 8)
+    // List(3, 6, 9, 12)
+    // 想要實現一個不確定的功能，但是這個功能的主要目的是將數據集中的每一條數據進行處理後
+    // 返回新的集合，但是這個處理的邏輯是不確定
+    // Scala 集合提供了可以進行自動轉換的操作，具體的轉換邏輯由開發人員提供
+    // List => logic => List
 
-    // 數據集的交集
-    println(list1.intersect(list2)) // List(3, 4)
-    println(set1.intersect(set2)) // Set(3, 4)
+//    def test(num: Int): Int = {
+//      num * 3
+//    }
+//    println(transform(list1, test))
+    // println(transform(list1, _ * 3))
 
-    // 數據集的並集
-    println(list1.union(list2)) // List(1, 2, 3, 4, 3, 4, 5, 6)
-    println(set1.union(set2)) // HashSet(5, 1, 6, 2, 3, 4)
+    // TODO map方法就是用於將數據集中的每一條數據進行轉換處理，獲取轉換後的新的數據集
+    //      List => map(logic) => List
+    //      將這樣的函數稱之為功能函數，實現特定的功能，但是功能的邏輯不確定
+    println(list1.map(_ * 3))
 
-    // 數據集的差集
-    println(list1.diff(list2)) // List(1, 2)
-    println(set1.diff(set2)) // Set(1, 2)
+  }
+  def transform(list: List[Int], f:(Int => Int)): List[Int] = {
+    val newList = ListBuffer[Int]()
 
-    // 拉鍊:將兩個數據集拉在一起
-    // 基本原則是將兩個數據集中相同的位置的數據拉在一起，形成對偶元組
-    // 如果兩個數據及的數據長度不相同，那麼會以最少的那個數據及為基礎進行拉鍊
-    println(list1.zip(list2))
-    println(set1.zip(set2))
+    list.foreach(
+      num => {
+        val result: Int = f(num)
+        newList.append(result)
+      }
+    )
+    newList.toList
   }
 }
