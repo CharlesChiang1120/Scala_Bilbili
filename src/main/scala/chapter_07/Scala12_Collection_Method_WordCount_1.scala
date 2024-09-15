718e3d2d51c3c361e767c970a7d501909df9d6f7
Charles Chiang <charleschiang2343@gmail.com>
2024-09-15 22:41:22 +0800

    chapter_07/Scala12_Collection_Method_WordCount_1

--- src/main/scala/chapter_07/Scala12_Collection_Method_WordCount_1.scala
+++ src/main/scala/chapter_07/Scala12_Collection_Method_WordCount_1.scala
@@ -9,29 +9,15 @@ object Scala12_Collection_Method_WordCount_1 {
     val dataList: List[String] = source.getLines().toList
     source.close()
 
-    val top3 = dataList
-      .flatMap(
-          line => {
-            line.split(" ")
-          }
-        ).map(
-            word => {
-              (word, 1)
-            }
-        ).groupBy (
-            kv => {
-              kv._1
-            }
-        ).mapValues(
-          list => {
-            list.size
-          }
-        ).toList.sortBy(
-          wc => {
-            wc._2
-          }
-        )(Ordering.Int.reverse).take(3)
+    dataList
+      .flatMap(_.split(" "))
+      .map((_, 1))
+      .groupBy (_._1)
+      .mapValues(_.size)
+      .toList
+      .sortBy(_._2)(Ordering.Int.reverse)
+      .take(3)
+      .foreach(println)
 
-    println(top3)
   }
 }
