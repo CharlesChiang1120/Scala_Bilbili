b8607b19cfe983c9bf29766f721c14ab9f604145
Charles Chiang <charleschiang2343@gmail.com>
2024-09-17 07:31:00 +0800

    chapter_07/Scala13_Collection_Method_WordCount_2

--- src/main/scala/chapter_07/Scala13_Collection_Method_WordCount_2.scala
+++ src/main/scala/chapter_07/Scala13_Collection_Method_WordCount_2.scala
@@ -1,31 +1,28 @@
 package chapter_07
 
-object Scala13_Collection_Method_WordCount_1 {
+object Scala13_Collection_Method_WordCount_2 {
   def main(args: Array[String]): Unit = {
 
     // TODO 使用功能函數實現 WordCount
 
     val list = List(
-      ("Hello World Scala Spark", 4),
-      ("Hello World Scala", 3),
-      ("Hello World", 2),
-      ("Hello", 1),
+      ("Hello", 1), ("Hello", 2), ("Hello", 3), ("Hello", 4)
     )
-    // 扁平化
-    // ("Hello World Scala", 3) => (Hello, 3), (World, 3), (Scala, 3)
-    // ("Hello World", 2) => (Hello, 2), (World, 2)
-    list.flatMap(
-      kv => {
-        val line = kv._1
-        val cnt = kv._2
-        val words = line.split(" ")
-        words.map(
-          word => (word, cnt)
-        )
-      }
-    ).groupBy(
-      kv => kv._1
-    ).foreach(println)
+
+    // ("Hello", 10)
+    // 集合可以進行簡單計算
+    //    val list1 = List(1, 2, 3, 4)
+    //    println(list1.sum) // 10
+    //    println(list1.max) // 4
+    //    println(list1.min) // 1
+    //    println(list1.product) // 24
+
+    val sum = list.map(
+      kv => kv._2
+    )
+
+    println(sum)
+
 
     // (Hello, 1), (Hello, 2), (Hello, 3), (Hello, 4)
     // (Hello, 10)
