184a7a41e7f986e6db74a5895d10fad299f09454
Charles Chiang <charleschiang2343@gmail.com>
2024-09-17 09:27:07 +0800

    chapter_07/Scala16_Collection_Par

---
+++ src/main/scala/chapter_07/Scala16_Collection_Par.scala
@@ -0,0 +1,17 @@
+package chapter_07
+
+import scala.collection.mutable
+
+object Scala16_Collection_Par {
+  def main(args: Array[String]): Unit = {
+
+    // TODO 集合
+    // par 方法可以將普通集合轉換成並行集合
+    // 並行集合
+    val result1 = (1 to 5).map{x => Thread.currentThread.getName}
+    //    val result2 = (1 to 5).toVector.partition().map{ x => Thread.currentThread.getName}
+
+    println(result1)
+    //    println(result2)
+  }
+}
