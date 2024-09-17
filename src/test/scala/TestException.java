58c53cc1e661b46a5a2e973a262fdc8e8c5aff7e
Charles Chiang <charleschiang2343@gmail.com>
2024-09-17 15:31:14 +0800

    src/test/scala/TestException.java

--- src/test/scala/TestException.java
+++ src/test/scala/TestException.java
@@ -2,7 +2,7 @@ import java.io.FileInputStream;
 
 public class TestException {
     public static void main(String[] args) throws Exception{
-         new FileInputStream("xxxxx");
+        new FileInputStream("xxxxx");
         String s = null;
         if ( s != null) {
             System.out.println(s.trim());
