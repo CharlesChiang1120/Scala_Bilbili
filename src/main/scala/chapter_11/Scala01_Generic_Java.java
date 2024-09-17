6fb744b834d463545d4752b53d20f5e6f5475bab
Charles Chiang <charleschiang2343@gmail.com>
2024-09-17 19:20:09 +0800

    chapter_11/Scala01_Generic_Java

---
+++ src/main/scala/chapter_11/Scala01_Generic_Java.java
@@ -0,0 +1,13 @@
+package chapter_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scala01_Generic_Java {
+    public static void main(String[] args) {
+        // TODO Java 中的泛型
+        // TODO 1. 泛型和類型的區別
+
+        List<String> strList = new ArrayList<String>();
+    }
+}
