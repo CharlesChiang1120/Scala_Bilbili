77c0d197924c7fe520c67d6a5b418765a1e5c0ac
Charles Chiang <charleschiang2343@gmail.com>
2024-09-17 15:37:09 +0800

    src/test/scala/TestException.java

--- src/test/scala/TestException.java
+++ src/test/scala/TestException.java
@@ -2,10 +2,21 @@ import java.io.FileInputStream;
 
 public class TestException {
     public static void main(String[] args) throws Exception{
-        new FileInputStream("xxxxx");
-        String s = null;
-        if ( s != null) {
-            System.out.println(s.trim());
+//        new FileInputStream("xxxxx");
+//        String s = null;
+//        if ( s != null) {
+//            System.out.println(s.trim());
+//        }
+        // 異常存在範圍的概念，所以捕捉異常也存在範圍的概念
+        // 異常地捕捉順序是從上到下，所以需要將範圍小的異常先捕捉，然後再捕捉範圍大的異常
+        try {
+            int i = 0;
+            // java.lang.ArithmeticException
+            int j = 10 / i;
+        } catch ( ArithmeticException e ) {
+            e.printStackTrace();
+        } catch ( Exception e ) {
+            e.printStackTrace();
         }
     }
 }
