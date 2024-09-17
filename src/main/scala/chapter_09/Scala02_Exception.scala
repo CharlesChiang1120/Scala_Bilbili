6f9c74b861b65d9c304e7910f052124ec75c5447
Charles Chiang <charleschiang2343@gmail.com>
2024-09-17 15:49:22 +0800

    chapter_09/Scala02_Exception

--- src/main/scala/chapter_09/Scala02_Exception.scala
+++ src/main/scala/chapter_09/Scala02_Exception.scala
@@ -8,18 +8,22 @@ object Scala02_Exception {
     // TODO 異常
     // Scala 中的異常處理類似於模式匹配中的類型匹配
     // 捕捉異常時，無需考慮異常的範圍，但是如果異常方法大先捕捉，其他的異常就捕捉不到，沒有意義
-    try {
-      val i = 0
-      val j = 10 /i
-    } catch {
-      case e : ArithmeticException => {
-        println("ArithmeticException: " + e.getMessage)
-      }
-      case e: Exception => {
-        println("Exception: " + e.getMessage)
-      }
-    } finally {
-
+//    try {
+//      val i = 0
+//      val j = 10 /i
+//    } catch {
+//      case e : ArithmeticException => {
+//        println("ArithmeticException: " + e.getMessage)
+//      }
+//      case e: Exception => {
+//        println("Exception: " + e.getMessage)
+//      }
+//    } finally {
+//
+//    }
+    @throws[Exception]
+    def test() = {
+      throw new Exception()
     }
   }
 }
