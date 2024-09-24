db6c4ec58b107d5466b9fde4b5690b027afef0e1
Charles Chiang <charleschiang2343@gmail.com>
2024-09-24 19:51:51 +0800

    Scala01_Generic_Java

--- src/main/scala/chapter_11/Scala01_Generic_Java.java
+++ src/main/scala/chapter_11/Scala01_Generic_Java.java
@@ -12,8 +12,8 @@ public class Scala01_Generic_Java {
         // String 稱之為泛型，用於約束內部數據的類型
         // 類型和泛型不是一個層次上的東西
 //        List<String> strList = new ArrayList<String>();
-        Message<String> message = new Message<String>();
-        final String content = message.content;
+//        Message<String> message = new Message<String>();
+//        final String content = message.content;
 
         // TODO 2. 泛型的聲明方式
         // 泛型用於約束內部數據的類型，所以需要從外部傳遞到類的內部
@@ -41,11 +41,27 @@ public class Scala01_Generic_Java {
 //        test(list);
 
         // TODO 6. 泛型不可變
-        Message<UserX> userMessage1 = new Message<UserX>();
+//        Message<UserX> userMessage1 = new Message<UserX>();
 //        Message<UserX> userMessage2 = new Message<Parent>(); (x)
 //        Message<UserX> userMessage3 = new Message<Child>(); (x)
 
+        // TODO 7. 如果泛型變化比較小，那麼應用面比較窄，所以泛型可以擴大範圍
+        //         使用過程中，泛型存在上限和下限的概念
+        // 使用數據的時候，如果類型查找的過程是往父類查找，表示使用通用性牆的類型
+        // 這個查找的過程會存在下限的概念
+        // TODO 下限一班應用於生產者對象
 
+        Producer<UserX> producer = new Producer<UserX>();
+        producer.produce(new Message<UserX>());
+        producer.produce(new Message<Parent>());
+//        producer.produce(new Message<Child>());
+
+        // 使用數據的時候，如果類型查找的過程是往子類查找，表示使用功能不丟失
+        // 這個查找的過程會存在上限的概念
+        // TODO 上限一班應用於消費者對象
+        Consumer<UserX> consumer = new Consumer<UserX>();
+        final Message<? extends  UserX> consume = consumer.consume();
+        final UserX content = consume.content;
 
     }
     public static void test(Collection<Object> list) {
@@ -53,6 +69,18 @@ public class Scala01_Generic_Java {
     }
 }
 
+class Consumer<T> {
+    // 上限，類型往下找
+    public Message<? extends T> consume() {
+        return null;
+    }
+}
+class Producer<T> {
+    // 下限，類型往上找
+    public void produce(Message<? super T> message) {
+
+    }
+}
 class Parent {
 
 }
