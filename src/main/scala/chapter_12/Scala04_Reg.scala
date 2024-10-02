706d87ce3e75a2b260abc9c75905ec4dd1c95d39
Charles Chiang <charleschiang2343@gmail.com>
2024-10-02 09:22:03 +0800

    Scala04_Reg

--- src/main/scala/chapter_12/Scala04_Reg.scala
+++ src/main/scala/chapter_12/Scala04_Reg.scala
@@ -1,18 +1,17 @@
 package chapter_12
 
-object Scala03_Reg {
+object Scala04_Reg {
   def main(args: Array[String]): Unit = {
     // TODO 正規表達式
 
-    // 3 + 8
-    val regex = "^((13[0-9])|(14[5,7,9])|(15[^4])|(18[0-9])|(17[0,1,3,5,6,7,8]))[0-9]{8}$".r
-    val str = "18845678901"
+    val email = "abc@gmail.com"
+    val regex = "(.*)(@.*)".r
 
-    val maybeString: Option[String] = regex.findFirstIn(str)
-    if ( maybeString.isEmpty) {
-      println("字符串不符合匹配規則")
-    } else {
-      println("字符串符合匹配規則: " + maybeString.get)
+    val maskedEmail = email match {
+      case regex(_, domain) => "*****" + domain
+      case _ => "Invalid email format"
     }
-  }
+
+    println(maskedEmail) // *****@gmail.com
+    }
 }
